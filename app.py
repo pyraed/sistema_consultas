55cc72b0756b09d3afeccef36939a07d1750db3d
Agustin Etcheto <a.etcheto@hotmail.com>
2026-04-29 23:22:20 -0300

    COMPLETO FICHA AFILIACION prueba 2

--- app.py
+++ app.py
@@ -444,8 +444,9 @@ def _texto_contrato(c, i: int, rep: str, datos: dict, cuota_prestamo: float):
 
     if i == 0:
         c.setFont("Helvetica", 10)
-        c.drawString(120, 620, datos["nombre"]) 
+        c.drawString(120, 620, datos["nombre"]) # ficha de afiliacion
         c.drawString(259, 95, datos["nombre"]) # Esta es la aclaracion
+        c.drawString(120, 265, datos["nombre"]) # En el texto
         c.drawString(160, 595, datos["fecha"])
         c.drawString(150, 550, datos["domicilio"])
         c.drawString(380, 525, datos["localidad"])
@@ -453,7 +454,10 @@ def _texto_contrato(c, i: int, rep: str, datos: dict, cuota_prestamo: float):
         c.drawString(80, 575, datos["email"])
         c.drawString(100, 505, datos["reparticion"])
         c.drawString(130, 460, datos["telefono"])
-        c.drawString(495, 95, datos["dni"])        
+        c.drawString(250, 525, datos["dni"]) # ficha de afiliacion
+        c.drawString(495, 95, datos["dni"])  # Esta es la aclaracion
+        c.drawString(270, 265, datos["dni"])  # En el texto
+
 
 
 def firmar_contrato(contrato_path: str, firma_buffer: io.BytesIO,
