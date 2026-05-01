d7505c78d70217093ad9230a24841c21c6427e39
Agustin Etcheto <a.etcheto@hotmail.com>
2026-04-30 21:11:57 -0300

    COMPLETO FICHA AFILIACION prueba 8

--- app.py
+++ app.py
@@ -421,11 +421,26 @@ def _texto_contrato(c, i: int, rep: str, datos: dict, cuota_prestamo: float):
         c.drawString(200, 630, datos["dni"])
         c.drawString(200, 615, datos["email"])
         c.drawString(400, 600, datos["telefono"])
-        c.drawString(120, 255, datos["nombre"])
-        c.drawString(150, 480, "Asociación de Apoyo a las Mujeres Argentinas Solas")
-        c.drawString(90, 243, datos["dni"])
-        c.drawString(350, 255, "Presidente")
-        c.drawString(320, 243, "20202020")
+
+
+        if i == 12:
+            c.setFont("Helvetica", 10)
+            c.drawString(120, 255, datos["nombre"])
+            c.drawString(180, 745, "Asociación de Apoyo a las Mujeres Argentinas Solas")
+            c.drawString(90, 243, datos["dni"])
+            c.drawString(350, 255, "Presidente")
+            c.drawString(320, 243, "20202020")
+
+        if i == 13:
+            c.setFont("Helvetica", 10)
+            c.drawString(180, 745, "Asociación de Apoyo a las Mujeres Argentinas Solas")
+            c.drawString(90, 390, datos["dni"])
+            c.drawString(120, 405, datos["nombre"])
+            c.drawString(350, 405, "Presidente")
+            c.drawString(320, 243, "20202020")
+
+
+
 
 
 
